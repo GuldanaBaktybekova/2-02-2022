90c3f34261db4156b3e26396386900599a3e99a8
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-02 15:00:31 +0600

    add fast modify

--- script.js
+++ script.js
@@ -48,3 +48,62 @@ var exampleObject2 = Math;
 
 
 // Output
+// alert("Hello world");
+// var result = prompt("Hello world"); string
+// var result = confirm("Hello world"); boolean
+// document.write("Hello world");
+
+
+var exampleTypeConversion1 = 5 + "5";
+var exampleTypeConversion2 = 5 + 5 + "5";
+var exampleTypeConversion3 = true + 2; //true=1
+var exampleTypeConversion4 = new String(5);
+var exampleTypeConversion5 = new Number("10");
+var exampleTypeConversion6 = new Boolean("");
+// document.write(exampleTypeConversion3);
+
+
+// Math
+// Operand x = 5 + 6;
+// Operands: 5,6
+// Operator: +
+
+// Unary operator
+var x = 5;
+x = -x;
+
+
+// var y = "3";
+// y = +y;
+
+// Binary operator
+var x = 1, y = 3;
+y = x + y;
+
+var exampleMath1 = 5 + 6;
+var exampleMath2 = 5 - 6;
+var exampleMath3 = 5 / 6;
+var exampleMath4 = 5 * 6;
+
+// Remainder
+var exampleMath5 = 5 % 2; //1
+var exampleMath6 = 6 % 2; //0
+
+// Exponent
+var exampleMath7 = 2 ** 4;
+
+// document.write(5 * (2 / 3) - 5);
+
+
+// Fast modify 
+var exampleMath8 = 4;
+exampleMath8 += 5;
+exampleMath8 -= 3;  //exampleMath8 = exampleMath8 - 3
+
+
+// Increment  / decrement
+var exampleMath9 = 0;
+exampleMath9++;
+exampleMath9--;
+++exampleMath9;
+--exampleMath9;
