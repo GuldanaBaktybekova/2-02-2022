c01679bbe806721c2f54408670a475720d9e960c
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-02 14:30:07 +0600

    add var

---
+++ script.js
@@ -0,0 +1,36 @@
+// Variables
+var emptyVariable;
+var message = "Hello world"
+var firstName, lastName = "ABC";
+
+// Nunmber
+// JS - математические операции 
+var exampleNumber1 = 123;
+var exampleNumber2 = 123.45;
+var exampleNumber3 = Infinity; //-Infinity
+var exmapleNumber4 = NaN;  //Not a Number
+
+// document.write(typeof exampleNumber1);
+
+
+
+// String
+var exampleString = "Hello world";
+var exampleString2 = 'Hello world';
+var exampleString3 = `Hello world`;
+var exampleString4 = "4";
+
+// Boolean
+var exampleBoolean1 = true;
+var exampleBoolean2 = false;
+
+
+// Special types
+var exampleNull = null;
+var exampleUNdefined1;
+var exampleUndefined2 = undefined;
+
+// document.write(exampleUNdefined);
+
+// Not now
+var exampleBigInt = 15161631615
